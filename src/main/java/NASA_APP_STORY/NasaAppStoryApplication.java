19697039e23c41d74c676c29ceba1ab169e9a32c
auguNa <augustin.nae@gmail.com>
2024-10-05 17:19:26 +0200

    first commit

---
+++ src/main/java/NASA_APP_STORY/NasaAppStoryApplication.java
@@ -0,0 +1,13 @@
+package NASA_APP_STORY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAppStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAppStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NASA_APP_STORY/NasaAppStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package NASA_APP_STORY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaAppStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
